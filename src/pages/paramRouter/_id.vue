88d70340c760d2028e90b1d733b4285944e1d64f
Fengsx <fengsx@yeah.net>
2022-09-15 20:56:17 +0800

    router参数示例：路径参数&URL参数

--- src/pages/paramRouter/_id.vue
+++ src/pages/paramRouter/_id.vue
@@ -6,7 +6,9 @@
 import { computed } from '@vue/reactivity';
 import { useRoute } from 'vue-router'
 const route = useRoute()
-console.log(route.params);
+
+// 路径参数 eg：paramRouter/id/1 => routeId=1
+// 需要路径参数的页面以文件名 ‘_’开头，路由会去除开头的 ‘_’
 const routeId = computed(() => {
     return route.params.id
 })
--- src/pages/paramRouter/urlParam.vue
+++ src/pages/paramRouter/urlParam.vue
@@ -6,6 +6,8 @@
 import { computed } from '@vue/reactivity';
 import { useRoute } from 'vue-router'
 const route = useRoute()
+
+// url参数 eg：urlParam?a=1&b=2
 const query = computed(() => {
     return route.query
 })
